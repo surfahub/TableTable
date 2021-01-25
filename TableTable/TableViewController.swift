8c3bb70ebb7b340bf442e12c738b575a152bad5d
Surfa <surfa451@gmail.com>
2021-01-25 00:44:06 -0500

    Initial Commit

---
+++ TableTable/TableViewController.swift
@@ -0,0 +1,53 @@
+   //
+//  TableViewController.swift
+//  TableTable
+//
+//  Created by Surfa on 24.01.2021.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+
+  
+
+    // MARK: - Table view data source
+
+    let restaurantNames = [
+            "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
+            "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
+            "Speak Easy", "Morris Pub", "Вкусные истории",
+            "Классик", "Love&Life", "Шок", "Бочка"
+        ]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return restaurantNames.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+        cell.textLabel?.text = restaurantNames[indexPath.row]
+        cell.imageView?.image = UIImage(named: restaurantNames[indexPath.row])
+       // cell.imageView?.bounds =
+
+                return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TableTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableTable
+//
+//  Created by Surfa on 24.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
