b9b58a1c9daab63c854f130a040bfbaf2ebcc809
Surfa <surfa451@gmail.com>
2021-02-02 04:21:11 +0300

    Custom TableViewCell

---
+++ TableTable/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  TableTable
+//
+//  Created by Surfa on 02.02.2021.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    
+}
